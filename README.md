698d6dc9fd34b24cc001c21c56a1ba220b91cb30
Anna Winther <annawiinther@gmail.com>
2020-01-05 14:00:35 +0100

    change directory layout and file names

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 A web app that let you choose a tv-show and gives you information about every episode (picture, episode name, summary, season and episode number).
 
 Uses tvmaze API
+
+command: npm install, to get the project up and running
+command: node server, to run project on port 8080
--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-// Setup empty JS object to act as endpoint for all routes
 const projectData = {};
-
-// Express to run server and routes
 const express = require('express');
-
-// Start up an instance of app
 const app = express();
 
 /* Dependencies */
 /* Middleware*/
 
-//Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+
 const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('../client/views'));
+app.use(express.static('webapp'));
 
 const port = 8080;
 
@@ -31,11 +25,8 @@ function listening() {
   console.log(`server running on localhost: ${port}`);
 }
 
-
-// Initialize all route with a callback function
 app.get('/all', callBack);
 
-// Callback function to complete GET '/all'
 function callBack(req, res){
   res.send(projectData);
 };
